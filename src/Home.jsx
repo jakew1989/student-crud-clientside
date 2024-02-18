b43a84e43007c7b5f57087ada73f62fac86ffee5
Jake Wright <jake.wright99@yahoo.com>
2024-02-18 11:41:40 +0000

    styled for mobile viewing

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,12 +9,11 @@ function Home() {
   const [data, setData] = useState([]);
     useEffect(() => {
         axios.get("https://mysql-student-crud-app-f552d548bcbd.herokuapp.com/")
-        .then(res =>  {
-          console.log(res);
-          (res => setData(res.data))
-  })
+        .then(res => setData(res.data))
         .catch(err => console.log(err))
     }, [])
+
+    console.log(`data is ${data}`)
 
     const handleDelete = (id) => {
       axios.delete("https://mysql-student-crud-app-f552d548bcbd.herokuapp.com/delete/" +id)
@@ -26,7 +25,7 @@ function Home() {
   return (
     <div className="d-flex vw-100 vh-100 bg-primary justify-content-center align-items-center">
     <div id="container" className='w-70 bg-white rounded p-3 position-relative'>
-      <div className='d-flex justify-content-between'>
+      <div className='d-flex justify-content-between title-container'>
         <h2>Student List</h2>
         <Link className='btn btn-success' to="/create">Create +</Link>
       </div>
@@ -42,13 +41,13 @@ function Home() {
         <tbody>
           {data.map((student, index) => {
             return <tr key={index}>
-              <td>{student[0].id}</td>
-              <td>{student[0].name}</td>
-              <td>{student[0].email}</td>
-              <td>
-              <Link to={`/read/${student.id}`} className='btn btn-sm btn-info'>Read</Link>
-              <Link to={`/edit/${student.id}`} className='btn btn-sm btn-primary mx-2'>Edit</Link>
-              <button onClick= { () => handleDelete(student.id)} className='btn btn-sm btn-danger'>Delete</button>
+              <td>{student.id}</td>
+              <td>{student.name}</td>
+              <td>{student.email}</td>
+              <td className='edit-button-container'>
+              <Link to={`/read/${student.id}`} className='btn btn-sm btn-info edit-button'>Read</Link>
+              <Link to={`/edit/${student.id}`} className='btn btn-sm btn-primary mx-2 edit-button'>Edit</Link>
+              <button onClick= { () => handleDelete(student.id)} className='btn btn-sm btn-danger edit-button'>Delete</button>
               </td>
             </tr>
           })}
